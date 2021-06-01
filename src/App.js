506184135096886c38fe7a98bf3cb79a60149922
krishna0379 <iamkrishna0379@gmail.com>
2021-06-01 20:26:09 +0530

    Router multipuel pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
-import { Button, ButtonGroup, Col, Row } from "react-bootstrap";
 import { Member1 } from "./views/Member1";
 import { Member2 } from "./views/Member2";
 import { Member3 } from "./views/Member3";
+import { Button, ButtonGroup, Col, Row } from "react-bootstrap";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
+//team leader
 function App() {
   return (
-    <div className="container-fulid">
-      <div className="row" style={{ height: "100vh" }}>
-        <div className="col-12 col-md-6 bg-dark text-light d-flex align-items-center justify-content-center mb-3">
-          <Member1 />
-        </div>
-        <div className="col-12 col-md-6 bg-info text-light d-flex align-items-center justify-content-center">
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Page1 />
+        </Route>
+
+        <Route path="/page1" exact>
+          <Page1 />
+          <Member1/>
+        </Route>
+
+        <Route path="/page2" exact>
+          <Page2/>
           <Member2 />
-        </div>
-      </div>
+        </Route>
+
+        <Route path="/page3" exact>
+          <Page3/>
+          <Member3 />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+function Page1() {
+  return (
+    <div>
+      <h1>Page1</h1>
     </div>
   );
 }
+function Page2() {
+  return (
+    <div>
+      <h1>Page2</h1>
+    </div>
+  );
+}
+function Page3() {
+  return (
+    <div>
+      <h1>Page3</h1>
+    </div>
+  );
+}
+ 
 export default App;
