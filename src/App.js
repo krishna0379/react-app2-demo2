81f3664daedb92f64867590f1f36147d8be7ce1c
krishna0379 <iamkrishna0379@gmail.com>
2021-06-01 16:36:29 +0530

    team leader

--- src/App.js
+++ src/App.js
@@ -2,145 +2,20 @@ import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
 import { Button, ButtonGroup, Col, Row } from "react-bootstrap";
+import { Member1 } from "./views/Member1";
+import { Member2 } from "./views/Member2";
+import { Member3 } from "./views/Member3";
 
 function App() {
   return (
-    <div>
-      <Member1 />
-      <div className="mb-5"></div>
-      <Member2 />
-      <div className="mb-5"></div>
-      <Member3 />
-    </div>
-  );
-}
-
-function Member1() {
-  return (
-    <div>
-      <h1>Member 1 Registeration</h1>
-      <div>
-        <div className="mb-2">
-          <input
-            type="text"
-            name=""
-            id=""
-            placeholder="Enter Username"
-            className="form-control"
-          />
+    <div className="container-fulid">
+      <div className="row" style={{ height: "100vh" }}>
+        <div className="col-12 col-md-6 bg-dark text-light d-flex align-items-center justify-content-center mb-3">
+          <Member1 />
         </div>
-
-        <div className="mb-2">
-          <input
-            type="text"
-            name=""
-            id=""
-            placeholder="Enter Password"
-            className="form-control"
-          />
+        <div className="col-12 col-md-6 bg-info text-light d-flex align-items-center justify-content-center">
+          <Member2 />
         </div>
-
-        <div className="mb-2">
-          <input
-            type="text"
-            name=""
-            id=""
-            placeholder="Enter Email"
-            className="form-control"
-          />
-        </div>
-
-        <div className="mb-2">
-          <input
-            type="text"
-            name=""
-            id=""
-            placeholder="Enter Mobile"
-            className="form-control"
-          />
-        </div>
-        <div className="mb-2">
-          <input
-            type="Button"
-            value="Register"
-            className="btn btn-primary w-100"
-          />
-        </div>
-      </div>
-    </div>
-  );
-}
-
-function Member2() {
-  return (
-    <div>
-      <h1>Member 2 - Login Module</h1>
-      <div className="mb-1">
-        <input
-          type="text"
-          placeholder="Enter Username"
-          className="form-control"
-        />
-      </div>
-
-      <div className="mb-1">
-        <input
-          type="text"
-          placeholder="Enter Password"
-          className="form-control"
-        />
-      </div>
-
-      <div>
-        <Button className="w-100">Login Here</Button>
-      </div>
-    </div>
-  );
-}
-
-function Member3() {
-  return (
-    <div>
-      <h1>Forgot Password Module</h1>
-
-      <div className="mb-1">
-        <input
-          type="text"
-          placeholder="Enter Username"
-          className="form-control"
-        />
-      </div>
-
-      <div className="mb-1">
-        <input
-          type="text"
-          placeholder="Old Password"
-          className="form-control"
-        />
-      </div>
-
-      <div className="mb-1">
-        <input
-          type="text"
-          placeholder="New Password"
-          className="form-control"
-        />
-      </div>
-
-      <div className="mb-1">
-        <input
-          type="text"
-          placeholder="Re Enter New Password"
-          className="form-control"
-        />
-      </div>
-
-      <div>
-        <input
-          type="button"
-          value="Reset Password"
-          className="btn btn-primary w-100"
-        />
       </div>
     </div>
   );
---
+++ src/views/Member1.js
@@ -0,0 +1,55 @@
+export function Member1() {
+  return (
+    <div>
+      <h1>Member 1 Registeration</h1>
+      <div>
+        <div className="mb-2">
+          <input
+            type="text"
+            name=""
+            id=""
+            placeholder="Enter Username"
+            className="form-control"
+          />
+        </div>
+
+        <div className="mb-2">
+          <input
+            type="text"
+            name=""
+            id=""
+            placeholder="Enter Password"
+            className="form-control"
+          />
+        </div>
+
+        <div className="mb-2">
+          <input
+            type="text"
+            name=""
+            id=""
+            placeholder="Enter Email"
+            className="form-control"
+          />
+        </div>
+
+        <div className="mb-2">
+          <input
+            type="text"
+            name=""
+            id=""
+            placeholder="Enter Mobile"
+            className="form-control"
+          />
+        </div>
+        <div className="mb-2">
+          <input
+            type="Button"
+            value="Register"
+            className="btn btn-primary w-100"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
